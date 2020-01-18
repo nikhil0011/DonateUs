0d2e8f6020efe2003951c1d595f7dc47147d44c1
Nikhil <nikhil.n@coolwinks.com>
2020-01-18 18:24:25 +0530

    remove string literal for paste action

--- DonateUs/View/BorderedTextField.swift
+++ DonateUs/View/BorderedTextField.swift
@@ -30,7 +30,7 @@ class BorderedTextField: UITextField {
         fatalError("init(coder:) has not been implemented")
     }
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == "paste:" {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
             return false
         }
         return super.canPerformAction(action, withSender: sender)
